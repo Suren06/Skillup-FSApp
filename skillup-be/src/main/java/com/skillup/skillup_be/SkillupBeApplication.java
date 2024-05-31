3134cec4ad4205d363f07df0c5f2ff11570455ad
Suren06 <suren.boyilla@gmail.com>
2024-05-31 10:12:29 +0800

    first commit

---
+++ skillup-be/src/main/java/com/skillup/skillup_be/SkillupBeApplication.java
@@ -0,0 +1,13 @@
+package com.skillup.skillup_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillupBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillupBeApplication.class, args);
+	}
+
+}
---
+++ skillup-be/src/test/java/com/skillup/skillup_be/SkillupBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillup.skillup_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillupBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
